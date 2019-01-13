8b070074fa0e564ba699ae3a8a9f7df1c33296c3
TPeterW <peterwangtao0@hotmail.com>
2019-01-12 22:59:14 -0500

    That is correct

--- config/setup.js
+++ config/setup.js
@@ -15,11 +15,11 @@ module.exports = function (app, config) {
 		extended: true
 	}));
 
-	app.use(function (_, res, next) {
+	app.options("/*", function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-		next();
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+		res.send(200);
 	});
 
 	const controllers = glob.sync(config.root + '/controllers/*.js');
